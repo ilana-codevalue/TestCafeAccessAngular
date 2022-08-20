2a1d4ca97789daddaddedee8f1705754e20dc7f5
Ilan Adasha <ilanx.adasha@intel.com>
2022-08-20 15:43:06 +0300

    final

--- e2e-testcafe-tests/tests/test.ts
+++ e2e-testcafe-tests/tests/test.ts
@@ -8,14 +8,11 @@ fixture`Test structure`
    
 
 test('Login test', async t => {
-    // is user logout? 
-    await homePage
-    .login(USERNAME, PASSWORS)
+    await homePage.login(USERNAME, PASSWORS);
     await t.expect(await homePage.lct_loginState.textContent).eql("loggedIn");
 })
 
 .before(async () => {
-    // init backend to start from a fresh point, for example:
     // logout user before test start
     // do other stuff..
     await appBackendApi.callLogout(USERNAME);
